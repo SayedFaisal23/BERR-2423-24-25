661a669249b3fbf20a5324f2ccc3ef5734750dcb
Sayed Faisal <b122310429@student.utem.edu.my>
2025-03-22 17:14:20 +0800

    second commit

--- index.js
+++ index.js
@@ -1,23 +1,25 @@
 const { MongoClient } = require('mongodb');
 
 async function main() {
-// Replace <connection-string> with your MongoDB URI
+//Replace <connection-string> with your MongoDB URI
 const uri = "mongodb://localhost:27017"
 const client = new MongoClient(uri);
 
 try {
+console.time("time");
 await client.connect();
 console. log("Connected to MongoDB!");
+console.timeEnd("time");
 
 const db = client.db("testDB");
 const collection = db.collection("users");
 
 // Insert a document
-await collection. insertOne({ name: "Alice", age: 25 });
+await collection. insertOne({ name: "Ali", age: 55 });
 console. log("Document inserted!") ;
 
 // Query the document
-const result = await collection. findOne({ name: "Alice" });
+const result = await collection. findOne({ name: "Ali" });
 console. log("Query result:", result);
 } catch (err) {
 console.error("Error:", err) ;
